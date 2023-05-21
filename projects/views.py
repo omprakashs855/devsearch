932295acce7ac673e4225401e3b1cd1f4cc81b8c
Singh Om Prakash (MS/ESX8-INT-XC) <omprakash.singh@in.bosch.com>
2023-05-21 11:08:48 +0530

    Rendering Data to templates

--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projectList = [
+    {
+        "id":"1",
+        "title": "Ecommerce Website",
+        "description": "Fully functional ecommerce website"
+    },
+    {
+        "id":"2",
+        "title": "Portfolio Website",
+        "description": "This was a project where I build out my portfolio"
+    },
+    {
+        "id":"3",
+        "title": "Social Network",
+        "description": "Awesome open source project I am still working on"
+    }
+]
+
 def projects(request):
-    return render(request, 'projects/projects.html')
+    page = "Hello, you are on the projects page"
+    return render(request, 'projects/projects.html', {'projectList': projectList})
 
 def project(request, pk):
-    return render(request, 'projects/single-project.html')
+    projectObj = None
+    for project in projectList:
+        if project['id'] == pk:
+            projectObj = project
+            break
+    return render(request, 'projects/single-project.html', {'projectObj': projectObj})
 
