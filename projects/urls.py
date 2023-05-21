7f441fb0699c43c8c08051ea8c645c4c9fdb05f6
Singh Om Prakash (MS/ESX8-INT-XC) <omprakash.singh@in.bosch.com>
2023-05-21 22:17:37 +0530

    Model forms

--- projects/urls.py
+++ projects/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     
     path('', views.projects, name="projects"),
     path('project/<str:pk>/', views.project, name="single-project"),
+    path('create-project/', views.createProject, name="create-project"),
+
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,24 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Project
-
-projectList = [
-    {
-        "id":"1",
-        "title": "Ecommerce Website",
-        "description": "Fully functional ecommerce website"
-    },
-    {
-        "id":"2",
-        "title": "Portfolio Website",
-        "description": "This was a project where I build out my portfolio"
-    },
-    {
-        "id":"3",
-        "title": "Social Network",
-        "description": "Awesome open source project I am still working on"
-    }
-]
+from .forms import ProjectForm
 
 def projects(request):
     page = "Hello, you are on the projects page"
@@ -30,3 +13,7 @@ def project(request, pk):
     # return render(request, 'projects/single-project.html', {'project': projectObj, 'tags': tags})
     return render(request, 'projects/single-project.html', {'project': projectObj})
 
+def createProject(request):
+    form = ProjectForm()
+    context = {'form': form}
+    return render(request, "projects/project_form.html", context)
