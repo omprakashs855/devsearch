3c480c5cc628408f02d838c03dce08a6a233759d
Singh Om Prakash (MS/ESX8-INT-XC) <omprakash.singh@in.bosch.com>
2023-05-23 18:47:08 +0530

    adding user to update image from page

--- projects/views.py
+++ projects/views.py
@@ -16,7 +16,7 @@ def project(request, pk):
 def createProject(request):
     form = ProjectForm()
     if request.method == 'POST':
-        form = ProjectForm(request.POST)
+        form = ProjectForm(request.POST, request.FILES)
         if form.is_valid(): #check the form if valid
             form.save() # save the data into database
             return redirect('projects') # redirect to homepage
@@ -28,7 +28,7 @@ def updateProject(request, pk):
     project = Project.objects.get(id=pk)
     form = ProjectForm(instance=project)
     if request.method == 'POST':
-        form = ProjectForm(request.POST, instance=project)
+        form = ProjectForm(request.POST, request.FILES, instance=project)
         if form.is_valid(): #check the form if valid
             note = form.save(commit=False) # to retain the data while editing
             note.save() # save the data into database
