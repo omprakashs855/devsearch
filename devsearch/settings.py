eaf33249906ae5b6f6d582bbfccb6094ed858bdd
Singh Om Prakash (MS/ESX8-INT-XC) <omprakash.singh@in.bosch.com>
2023-05-23 18:16:54 +0530

    Adding image routing and static files config

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -121,7 +121,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
+
+MEDIA_URL = '/images/'
--- projects/models.py
+++ projects/models.py
@@ -5,6 +5,7 @@ import uuid
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
+    featured_image = models.ImageField(null=True, blank=True, default='default.jpg')
     demo_link = models.CharField(null=True, blank=True, max_length=2000)
     source_link = models.CharField(null=True, blank=True, max_length=2000)
     tags = models.ManyToManyField('Tag', blank=True) # For many to many Relationship
