4499e97e938b139938fc2ade2fc10a784b638880
Singh Om Prakash (MS/ESX8-INT-XC) <omprakash.singh@in.bosch.com>
2023-05-27 17:55:55 +0530

    add and render users profile

--- users/models.py
+++ users/models.py
@@ -7,6 +7,7 @@ import uuid
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True) # OnetoOneMapping
     username = models.CharField(max_length=200, blank=True, null=True)
+    location = models.CharField(max_length=200, blank=True, null=True)
     name = models.CharField(max_length=200, blank=True, null=True)
     email = models.EmailField(max_length=500, blank=True, null=True)
     short_intro = models.CharField(max_length=200, blank=True, null=True)
@@ -23,4 +24,15 @@ class Profile(models.Model):
     
     def __str__(self):
         return str(self.user.username)
+    
+class Skill(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    description = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                           primary_key=True, editable=False)
+    
+    def __str__(self):
+        return str(self.name)
 
--- users/views.py
+++ users/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render, redirect
+from .models import Profile
 
 # Create your views here.
 
 def profiles(request):
-    context = {}
-    return render(request, 'users/profiles.html', context)
+    profiles = Profile.objects.all()
+    return render(request, 'users/profiles.html', {'profiles': profiles})
+
+def userProfile(request, pk):
+    profile = Profile.objects.get(id=pk)
+
+    topSkills = profile.skill_set.exclude(description__exact="")
+    otherskills = profile.skill_set.filter(description="")
+    context = {'profile': profile, "topSkills": topSkills, "otherskills": otherskills}
+    return render(request, 'users/user-profile.html', context)
