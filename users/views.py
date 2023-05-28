0a57fae4f6d43d765f8e5c320e6b6804739dd4d7
Singh Om Prakash (MS/ESX8-INT-XC) <omprakash.singh@in.bosch.com>
2023-05-28 14:35:09 +0530

    adding user registration

--- users/views.py
+++ users/views.py
@@ -4,10 +4,12 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 
 def loginUser(request):
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('profiles')
     if request.method == 'POST':
@@ -32,6 +34,24 @@ def logoutUser(request):
     logout(request)
     messages.success(request, 'Username was logged out')
     return redirect('login')
+
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)        # it is used to hold the instance of the form
+            user.username = user.username.lower() # to be modified 
+            user.save()                           # before it is saved
+
+            messages.success(request, "User account was created!")
+            login(request, user)
+            return redirect('profiles')
+        else:
+            messages.error(request, "An error has occured during registration")
+    context = {'page': page, 'form': form}
+    return render(request, 'users/login_register.html', context)
 
 def profiles(request):
     profiles = Profile.objects.all()
