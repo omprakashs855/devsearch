8ce4025a2624c1d4839b0af8016bf9fdb2ea1951
Singh Om Prakash (MS/ESX8-INT-XC) <omprakash.singh@in.bosch.com>
2023-05-28 20:45:12 +0530

    styling login and register page

--- static/uikit/app.js
+++ static/uikit/app.js
@@ -2,3 +2,14 @@
 document.addEventListener('DOMContentLoaded', function () {
   hljs.highlightAll();
 });
+
+let alertWrapper = document.querySelector('.alert')
+let alertClose = document.querySelector('.alert__close')
+
+if (alertWrapper) {
+  for (let i=0; i < alertClose.length; i++) {
+    alertClose.addEventListener('click', () => 
+    alertWrapper.style.display='none'  
+  )
+  }
+}
