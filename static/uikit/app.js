654947a2b6e5199f58b613c77c16bb07710d97cf
Singh Om Prakash (MS/ESX8-INT-XC) <omprakash.singh@in.bosch.com>
2023-05-30 19:23:05 +0530

    fixing bug for register and flashing alert

--- static/uikit/app.js
+++ static/uikit/app.js
@@ -3,13 +3,12 @@ document.addEventListener('DOMContentLoaded', function () {
   hljs.highlightAll();
 });
 
-let alertWrapper = document.querySelector('.alert')
-let alertClose = document.querySelector('.alert__close')
 
-if (alertWrapper) {
-  for (let i=0; i < alertClose.length; i++) {
-    alertClose.addEventListener('click', () => 
-    alertWrapper.style.display='none'  
-  )
-  }
+let alertWrapper = document.querySelector('.alert');
+let alertClose = document.querySelector('.alert__close');
+
+if (alertWrapper && alertClose) {
+  alertClose.addEventListener('click', () => {
+    alertWrapper.style.display = 'none';
+  });
 }
