478041ccec81f79667bf2c29a31443172f0756c0
Singh Om Prakash (MS/ESX8-INT-XC) <omprakash.singh@in.bosch.com>
2023-06-01 19:17:17 +0530

    messages

--- users/urls.py
+++ users/urls.py
@@ -12,4 +12,7 @@ urlpatterns = [
     path('delete-skill/<str:pk>', views.deleteSkill, name='delete-skill'),
     path('', views.profiles, name='profiles'),
     path('profile/<str:pk>', views.userProfile, name='user-profile'),
+    path('inbox/', views.inbox, name='inbox'),
+    path('message/<str:pk>', views.viewMessage, name='message'),
+    path('create-message/<str:pk>', views.createMessage, name='create-message'),
 ]
