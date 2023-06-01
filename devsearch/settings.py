17d92dfd69f41d79326e5d92e19fa91ca0b81793
Om Prakash Singh <os721688@gmail.com>
2023-06-01 22:31:33 +0530

    sending welcoming emails

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -117,6 +117,15 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Email Configuration
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'omprakashdjango@gmail.com'
+EMAIL_HOST_PASSWORD = "ievqfhgsdnnhjmye"
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
