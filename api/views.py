9871aabebc6a3a3890f344c652bbc7896239ebf9
Om Prakash Singh <os721688@gmail.com>
2023-06-02 22:02:04 +0530

    auth with Json web tokens

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .serializers import ProjectSerializer
 from projects.models import Project
@@ -16,7 +17,9 @@ def getRoutes(request):
     return Response(routes)
 
 @api_view(['GET'])
+# @permission_classes([IsAuthenticated]) # To setup a restricted route
 def getProjects(request):
+    print('USER:', request.user)
     projects = Project.objects.all()
     serializer = ProjectSerializer(projects, many=True)
     return Response(serializer.data)
